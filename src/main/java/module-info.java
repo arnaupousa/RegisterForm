02e43ee7ac0e54021e365b299a125bc9a5abe255
Arnau Pousa <pousa.arnau@gmail.com>
2026-03-19 11:43:47 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.arnau.registerformarnau {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.arnau.registerformarnau.controller to javafx.fxml;
-    
-    exports com.arnau.registerformarnau;
-}
